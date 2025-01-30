a229246f4fd2e6a4bdc058742b1394a6ad510021
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-01-30 20:51:21 +0530

    Custom type define in TS

--- One/index.ts
+++ One/index.ts
@@ -2,3 +2,20 @@ let uame: string = "adi";
 let numberofSame: number = 33;
 
 let isTrue: boolean = true;
+
+type Person = {
+  user: string;
+  age: number;
+  isStudent: boolean;
+};
+let person1: Person = {
+  user: "aditya",
+  age: 32,
+  isStudent: false,
+};
+
+let peroson2: Person = {
+  user: "sahil",
+  age: 21,
+  isStudent: true,
+};
