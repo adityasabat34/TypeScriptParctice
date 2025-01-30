75c637a9710dd97a8cdda7a2b1227877e13835a1
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-01-30 21:49:14 +0530

    Nested object and optional object type in TS

--- One/index.ts
+++ One/index.ts
@@ -3,19 +3,51 @@ let numberofSame: number = 33;
 
 let isTrue: boolean = true;
 
+////////////////////////////////////////////////////////
+
+type Address = {
+  street: string;
+  city: string;
+  country: string;
+};
+
 type Person = {
   user: string;
   age: number;
   isStudent: boolean;
+  address: Address;
 };
 let person1: Person = {
   user: "aditya",
   age: 32,
   isStudent: false,
+  address: {
+    street: "Chmebuty",
+    city: "Mumbai",
+    country: "India",
+  },
 };
 
 let peroson2: Person = {
   user: "sahil",
   age: 21,
   isStudent: true,
+  address: {
+    street: "Chmebuty",
+    city: "Mumbai",
+    country: "India",
+  },
 };
+
+//////////////////////////////////////////////////////////////////////////
+type PizzaDataType = {
+  name: string;
+  price: number;
+};
+
+let menu: PizzaDataType[] = [
+  {
+    name: "Maghaerita",
+    price: 200,
+  },
+];
