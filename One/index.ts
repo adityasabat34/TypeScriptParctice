e161a5e89ad0aef04a298b8de1eed6cac9716fd9
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-02-02 20:11:51 +0530

    literal type functional implementation

--- One/index.ts
+++ One/index.ts
@@ -1,3 +1,71 @@
-type username = "adi" | "sahil" | "ashish";
+type Pizza = {
+  id: number;
+  name: string;
+  price: number;
+};
 
-let user: username = "adi";
+type Order = {
+  id: number;
+  pizza: Pizza;
+  status: string;
+};
+
+const menu: Pizza[] = [
+  { id: 1, name: "margherita", price: 200 },
+  { id: 2, name: "Ciekn", price: 200 },
+  { id: 3, name: "margha", price: 200 },
+  { id: 4, name: "mta", price: 200 },
+  { id: 5, name: "margita", price: 200 },
+];
+
+// let orderQueue = [];
+
+let cashInRegister = 100;
+let nextOrderId = 1;
+const orderHistory: Order[] = [];
+
+function addNewPizza(pizzaObj: Pizza) {
+  menu.push(pizzaObj);
+}
+
+function placeOrder(pizzaName: string) {
+  const selectedPizza = menu.find((pizzaObj) => pizzaObj.name === pizzaName);
+  if (!selectedPizza) {
+    console.error(`${pizzaName} does not exist in the menu`);
+    return;
+  }
+  cashInRegister += selectedPizza.price;
+  const newOrder = { id: nextOrderId, pizza: selectedPizza, status: "ordered" };
+  nextOrderId++;
+  orderHistory.push(newOrder);
+  return newOrder;
+}
+
+function completeOrder(orderId: number) {
+  const order = orderHistory.find((order) => order.id === orderId);
+  if (!order) {
+    console.error(`${orderId} was not found in the orderQueue`);
+    return;
+  }
+  order.status = "completed";
+  return order;
+}
+
+addNewPizza({ name: "chicken", price: 300 });
+
+placeOrder("chicken");
+completeOrder(4);
+
+console.log(menu);
+console.log(cashInRegister);
+console.log(orderHistory);
+
+const getPizzaDetails = (identifier: string | number) => {
+  if (typeof identifier === "string") {
+    return menu.find(
+      (pizza) => pizza.name.toLocaleLowerCase() === identifier.toLowerCase()
+    );
+  } else {
+    return menu.find((pizza) => pizza.id === identifier);
+  }
+};
