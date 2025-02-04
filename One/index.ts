08d92fb34f8b064edae0c35a43a369397142211f
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-02-04 20:16:06 +0530

    Utility types in TS

--- One/index.ts
+++ One/index.ts
@@ -1,19 +1,29 @@
-type userRole = "guest" | "member" | "admin";
-
-type user = {
+type User = {
+  id: number;
   username: string;
-  role: userRole;
+  role: "member" | "contributor" | "admin";
 };
 
-const users: user[] = [
-  { username: "john doe", role: "member" },
-  { username: "jane doe", role: "admin" },
+type updatedUser = {
+  id?: number;
+  username?: string;
+  role?: "member" | "contributor" | "admin";
+};
+
+const users: User[] = [
+  { id: 1, username: "aditya", role: "admin" },
+  { id: 1, username: "sahil", role: "admin" },
+  { id: 1, username: "adi", role: "admin" },
+  { id: 1, username: "aditi", role: "admin" },
+  { id: 1, username: "amit", role: "admin" },
 ];
 
-function fetchUserDetails(username: string): user {
-  const user = users.find((user) => user.username === username);
-  if (!user) {
-    throw new Error("user not exist");
+function updateUser(id: any, upadtes: updatedUser) {
+  const userID = users.find((user) => user.id === id);
+  if (!userID) {
+    console.error("User not found");
+    return;
   }
-  return user;
+  Object.assign(userID, upadtes);
 }
+updateUser(1, { username: "aditya" });
